7d08b1f510c600074110b8826d53e96cc5c586b9
Kimble - Cpoiares <carlos.poiares2@gmail.com>
2020-01-07 23:26:49 +0000

    fixarino e ja funfa

--- src/background2.js
+++ src/background2.js
@@ -1,20 +1,30 @@
-var debug = true;
+var debug = false;
 var switcher = '/ltng/switcher?destination=classic&referrer=%2Flightning%2Fpage%2Fhome';
 
 chrome.browserAction.onClicked.addListener(function(tab) {    
     // URL = tab.url;
     if(debug)
-        parseURL("www.google.com/asd/asdsd/aasd12345678911");
+        parseURL("https://kimble-lma.lightning.force.com/lightning/r/KimbleOne__Resource__c/a8k3A000000LWXuQAO/view");
     else
-        parseURL(tabs.url);
+        parseURL(tab.url);
 });
 
-function parseURL(url)
-{
-    var domain, split_url = null, str = '', obj_id = "Never Populated", control = false;
-    var regex = new RegExp("(?=.*)[(a-z)+(0-9)+]{15,18}$");
 
-    split_url = url.split('/');
+
+
+async function parseURL(url)
+{
+    var domain, anotha_url = '', split_url = null, str = '', obj_id = "Never Populated", control = false, aux;
+    
+    var regex = new RegExp("(?=.*)[(a-zA-Z)+(0-9)+]{15,19}$");
+
+    alert("URL 1: " + url);
+    // Clear HTTPS://
+    aux = url.split('://')[1];
+
+    alert("URL 2: " + aux);
+    split_url = aux.split('/');
+
     domain = split_url[0];
 
     
@@ -34,13 +44,24 @@ function parseURL(url)
     }
 
     alert("Domain: " + domain + "\nObject ID found: " + obj_id + "\n");
-    var newURL = domain + '/' + switcher;
+    var newURL = "https://" + domain  + switcher;
     alert(newURL);
-    window.location.href = newURL;
-    
-    chrome.tabs.create({url: newURL});
 
-    // New url gets appended chrome-extension://id/
-    
+    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+        chrome.tabs.update(tab.id, {url: newURL});
+    });
+
+
+    await new Promise(r => setTimeout(r, 10000));
+
+    chrome.tabs.query({currentWindow: true, active: true}, function (tab_2) {
+
+        var aux = tab_2[0].url + '';
+        aux = aux.split("://")[1];
+        anotha_url = "https://" + aux.split('/')[0] + '/' + obj_id;
+        alert("Tab URL " + tab_2[0].url);
+        chrome.tabs.update(tab_2[0].id, {url: anotha_url});
+    });
+
 
 }
