fa3f8fca8f419d4c4bfa674ec4362932f584b7a4
Kimble - Cpoiares <carlos.poiares2@gmail.com>
2020-01-09 15:28:42 +0000

    Job Page fix

--- src/background.js
+++ src/background.js
@@ -1,3 +1,12 @@
+/* TODO: https://westmonroepartners.lightning.force.com/one/one.app#eyJjb21wb25lbnREZWYiOiJvbmU6YWxvaGFQYWdlIiwiYXR0cmlidXRlcyI6eyJhZGRyZXNzIjoiaHR0cHM6Ly93ZXN0bW9ucm9lcGFydG5lcnMubGlnaHRuaW5nLmZvcmNlLmNvbS9hcGV4L0tpbWJsZU9uZV9fQWN0aXZpdHlBc3NpZ25tZW50c0RlbGl2ZXJ5P2ZpbHRlcmlkPWE2d2YyMDAwMDAxNUdHbEFBTSZpZD1hNDlmMjAwMDAwMHB3WmdBQUkifSwic3RhdGUiOnt9fQ%3D%3D
+        switch to classic in new tab
+        open same page in new window
+
+    TODO: Correct Job Page 
+    if link: https://boardcrm.lightning.force.com/lightning/r/KimbleOne__Job__c/a1a0J0000069c3GQAQ/view
+    Calls page list view instead of ID
+
+*/
 const switcher = '/ltng/switcher?destination=classic&referrer=%2Flightning%2Fpage%2Fhome';
 var control = false;
 
@@ -45,6 +54,9 @@ function DealObjId(url)
 
     var domain, lightning_url = '', split_url = null, str = '', obj_id = "Never Populated", aux;
     
+    // Need to Correc Regex because 
+    // https://boardcrm.lightning.force.com/one/one.app#eyJjb21wb25lbnREZWYiOiJvbmU6YWxvaGFQYWdlIiwiYXR0cmlidXRlcyI6eyJhZGRyZXNzIjoiaHR0cHM6Ly9ib2FyZGNybS5saWdodG5pbmcuZm9yY2UuY29tL2FwZXgvS2ltYmxlT25lX19BY3Rpdml0eUFzc2lnbm1lbnRzRGVsaXZlcnk%2FZmlsdGVyaWQ9YTJ0MEowMDAwMDJBMkdKUUEwJmlkPWEwdDBKMDAwMDBFMnBVbFFBSiJ9LCJzdGF0ZSI6e319
+    // is picked up 
     const regex = new RegExp("(?=.*)[/(a-zA-Z)+(0-9)+]{16,19}$");
 
     // Clear HTTPS://
@@ -225,7 +237,8 @@ function DealJobPage(url)
 {
    // Classic argument for job page /apex/KimbleOne__ObjectLinks?f=KimbleOne__Job__C
 
-   var domain, classic_url = '', split_url = null, temp;
+   var domain, classic_url = '', split_url = null, temp, obj_id = "Never Populated";
+   const regex = new RegExp("(?=.*)[/(a-zA-Z)+(0-9)+]{16,19}$");
 
    // Clear HTTPS://
    temp = url.split('://')[1];
@@ -235,27 +248,45 @@ function DealJobPage(url)
    var newURL = "https://" + domain  + switcher;
    //alert(newURL);
 
-   chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-       chrome.tabs.update(tab.id, {url: newURL});
-   });
-
-   // Page is vsforce, need to split url by the '.' get the node and build url in form https://kimbleagent.node.visual.force.com/apex/AgentDashboard
-   chrome.webNavigation.onCompleted.addListener(function(details) {
-       if(!control){
-           chrome.tabs.query({currentWindow: true, active: true}, function (tab_classic) {
-               control = true;
-               var temp = tab_classic[0].url + '';
-               temp = temp.split("://")[1];
-               classic_url = "https://" + temp.split('/')[0] + "/apex/KimbleOne__ObjectLinks?f=KimbleOne__Job__C";
-               chrome.tabs.update(tab_classic[0].id, {url: classic_url});
-           });
+   for (const str of split_url)
+   {
+       if(regex.test(str))
+       {
+           obj_id = str;
+           break;
        }
-   }, {
-       url: [{
-           // Runs on example.com, example.net, but also example.foo.com
-           hostContains: '.salesforce.com'
-       }],
-   });
+   }
+
+   if(obj_id != "Never Populated")
+   {
+       DealObjId(url);
+   }
+   else
+   {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+        chrome.tabs.update(tab.id, {url: newURL});
+    });
+ 
+    // Page is vsforce, need to split url by the '.' get the node and build url in form https://kimbleagent.node.visual.force.com/apex/AgentDashboard
+    chrome.webNavigation.onCompleted.addListener(function(details) {
+        if(!control){
+            chrome.tabs.query({currentWindow: true, active: true}, function (tab_classic) {
+                control = true;
+                var temp = tab_classic[0].url + '';
+                temp = temp.split("://")[1];
+                classic_url = "https://" + temp.split('/')[0] + "/apex/KimbleOne__ObjectLinks?f=KimbleOne__Job__C";
+                chrome.tabs.update(tab_classic[0].id, {url: classic_url});
+            });
+        }
+    }, {
+        url: [{
+            // Runs on example.com, example.net, but also example.foo.com
+            hostContains: '.salesforce.com'
+        }],
+    });
+ 
+   }
+
 }
 
 
